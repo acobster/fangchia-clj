fe1d37969a49116f26daca5655feb1932591f9ff
The Ambassador of Funk <acobster@gmail.com>
2016-06-01 09:37:32 -0700

    comments

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -9900,6 +9900,10 @@ window.addEventListener( 'load', function() {
 
   jQuery('.responsive-nav').responsiveNav();
 
+
+  /* Banner logo */
+
+  // Push the FANG CHIA logo up into the corner when the user scrolls down
   window.addEventListener( 'scroll', function() {
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     if( scrollTop === 0 ) {
@@ -9909,10 +9913,16 @@ window.addEventListener( 'load', function() {
     }
   });
 
+  // Corner the logo if the page loaded partially scrolled
+  if ((document.documentElement.scrollTop || document.body.scrollTop) > 0) {
+    document.getElementsByClassName('site-heading-wrapper')[0].classList.add('corner');
+  }
+
 
 
   /* Bandcamp player */
 
+  // toggle open/close on click
   document.getElementById('open-player-btn').addEventListener('click', function() {
     var wrapper = document.getElementById('player-wrapper');
 
@@ -9925,8 +9935,11 @@ window.addEventListener( 'load', function() {
     }
   });
 
+  // bounce the player button on first visit
   if (!window.localStorage.repeatVisit) {
     var btnWrapper = document.getElementById('open-player-btn-wrapper');
+
+    // bouncy-bounce
     btnWrapper.classList.add('bouncy');
     window.localStorage.repeatVisit = true;
   }
